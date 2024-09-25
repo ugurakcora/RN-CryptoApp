aa00619e4f503bc7b8eec088fe036132ec85927b
ugurakcora <uakcora97@gmail.com>
2024-09-25 03:59:05 +0300

    feat: tabs

---
+++ src/screens/tabs/market/MarketScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function MarketScreen() {
+  return (
+    <View>
+      <Text>MarketScreen</Text>
+    </View>
+  );
+}
